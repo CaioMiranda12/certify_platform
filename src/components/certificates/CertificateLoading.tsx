ca5cb0a3ef6f5a70c01e6a8649c99f3d3d46ade7
CaioMiranda12 <caioclm04@gmail.com>
2026-09-19 22:21:19 -0300

    feat: create certificate loading component

---
+++ src/components/certificates/CertificateLoading.tsx
@@ -0,0 +1,7 @@
+export function CertificateLoading() {
+  return (
+    <div className="flex min-h-[570px] items-center justify-center">
+      <p className="text-sm text-[#111111]/40">Carregando certificados...</p>
+    </div>
+  );
+}
